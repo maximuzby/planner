ee173f97bd9ed77c73b5b74d9c6c5f3940ea2e6d
Max Bondarenko <maximuz.by@gmail.com>
2019-08-11 18:29:02 +0300

    Updates

---
+++ precache-manifest.cfa3e1702cf56c15140c5ce2d0cd2e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfa8ee5d1b7f7b0b1c5d540e572088a",
+    "url": "/planner/index.html"
+  },
+  {
+    "revision": "b1b5c57c9554cfdbcc38",
+    "url": "/planner/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "65d33edce629bdffc06b",
+    "url": "/planner/static/css/main.f32677c6.chunk.css"
+  },
+  {
+    "revision": "b1b5c57c9554cfdbcc38",
+    "url": "/planner/static/js/2.ad735749.chunk.js"
+  },
+  {
+    "revision": "65d33edce629bdffc06b",
+    "url": "/planner/static/js/main.61fab6c1.chunk.js"
+  },
+  {
+    "revision": "3a31fc4d09d6a3c4ebf9",
+    "url": "/planner/static/js/runtime~main.cd827cdc.js"
+  }
+]);
